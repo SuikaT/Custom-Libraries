b030673b9f5a4142fc9de81d8ad7194432b210fe
SuikaT <bourney.julien@gmail.com>
2025-02-26 00:16:19 +0100

    table

--- projects/ngx-custom-table/src/lib/custom-table.component.ts
+++ projects/ngx-custom-table/src/lib/custom-table.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from "@angular/core";
 
 @Component({
-    selector: "ngx-custom-table",
-    imports: [],
-    templateUrl: "custom-table.component.html",
-    styleUrl: "custom-table.component.scss"
+	selector: "ngx-custom-table",
+	standalone: true,
+	imports: [],
+	templateUrl: "custom-table.component.html",
+	styleUrl: "custom-table.component.scss",
 })
 export class CustomTableComponent {
 	@Input()
