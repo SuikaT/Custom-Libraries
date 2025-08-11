d92f9cda5e2407788f5b3187c1e7cb34ba313b2e
SuikaT <bourney.julien@gmail.com>
2025-08-12 01:29:19 +0200

    Lib add

---
+++ projects/suik-ui/src/lib/suik-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-suik-ui',
+  imports: [],
+  template: `
+    <p>
+      suik-ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class SuikUiComponent {
+
+}
---
+++ projects/suik-ui/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of suik-ui
+ */
+
+export * from './lib/suik-ui.service';
+export * from './lib/suik-ui.component';
